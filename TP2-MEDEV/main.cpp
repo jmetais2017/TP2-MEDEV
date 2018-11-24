3063e855e173c137f4364336cf92442cb3ed78a2
Julien <Julien@Asus>
2018-11-25 00:45:07 +0100

    main

--- TP2-MEDEV/main.cpp
+++ TP2-MEDEV/main.cpp
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: Julien
- *
- * Created on 25 novembre 2018, 00:10
- */
 
 #include <cstdlib>
 #include <iostream>
@@ -20,7 +8,7 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    cout << "github" << endl;
+    cout << "main" << endl;
 
     return 0;
 }

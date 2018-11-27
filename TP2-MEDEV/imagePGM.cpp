aa16180ce0fed3be589aecbd31b733b5fb5879eb
eleve <eleve@130.66.192.8>
2018-11-27 17:46:08 +0100

    .cpp avec lecture/ecriture

--- TP2-MEDEV/imagePGM.cpp
+++ TP2-MEDEV/imagePGM.cpp
@@ -1,18 +1,25 @@
 
 #include "imagePGM.h"
 
-//Constructeurs
-imagePGM::imagePGM(){}
 
-imagePGM::imagePGM(imagePGM image){
+//Constructeurs
+imagePGM::imagePGM(){
+
+}
+
+imagePGM::imagePGM(imagePGM const& image){
     largeur = image.getLargeur();
     hauteur = image.getHauteur();
+    pixels = vector<vector<int>>();
     for (int i=0; i<hauteur ; i++){
+        pixels.push_back(vector<int>());
         for (int j=0; j<largeur ; j++){
-            pixels[i][j] = image.getPixel(i, j);
+            pixels[i].push_back(image.getPixel(i, j));
         }
     }
 }
+
+
 
 //Accesseurs
 int imagePGM::getLargeur() const{
@@ -38,4 +45,82 @@ void imagePGM::setLargeur(int const largeur){
 
 void imagePGM::setPixel(int const i, int const j, int const couleur){
     pixels[i][j] = couleur;
+}
+
+
+
+//Lecture
+
+void imagePGM::lecture(){
+    string chaine;
+    string nomFichier;
+    fstream fichier;
+    
+    cout << "Nom du fichier à lire? " << endl;
+    cin >> nomFichier ;
+    
+    fichier.open(nomFichier, ios::in);
+    cout << "Fichier ouvert " << endl;
+    
+    fichier >> chaine ; //P2
+    
+    fichier >> chaine ; //Ligne de commentaires
+    
+    
+    //Dimensions
+    fichier >> chaine ;
+    largeur = stoi(chaine);
+    fichier >> chaine ;
+    hauteur = stoi(chaine);
+    
+    fichier >> chaine ; //255
+    
+    //Pixels
+    cout << "Lecture des pixels " << endl;
+    pixels = vector<vector<int>>();
+    pixels.push_back(vector<int>());
+    int i = 0;
+    int j = 0;
+    while (!fichier.eof()){
+        fichier >> chaine ;
+        pixels[i].push_back(stoi(chaine));
+        j++;
+        if(j >= largeur){
+            j = 0;
+            i++;
+            pixels.push_back(vector<int>());
+        }
+        
+    }
+    fichier.close();
+  
+}
+
+
+//Ecriture
+
+void imagePGM::ecriture(){
+    fstream fichier;
+    string nomFichier;
+    
+    cout << "Nom du fichier dans lequel écrire? " << endl;
+    cin >> nomFichier ;
+    
+    fichier.open(nomFichier, ios::app|ios::out);
+    
+    fichier << "P2" << endl;
+    fichier << "#" << endl;
+    fichier << largeur << " " << hauteur << endl;
+    fichier << 255 << endl;
+    
+    //Ecriture des pixels
+    
+    for (int i=0; i<hauteur; i++){
+        for(int j=0; j<largeur; j++){
+            fichier << pixels[i][j] << " "; 
+        }
+        fichier << endl;
+    }
+    
+    fichier.close();
 }
